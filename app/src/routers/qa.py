9bad45cdcb34a39f508ec8916d7f5c56b4180661
Artyom Eryomkin <43377589+RakePants@users.noreply.github.com>
2024-05-07 10:57:52 +0500

    Add bot is typing

--- app/src/routers/qa.py
+++ app/src/routers/qa.py
@@ -1,5 +1,6 @@
 import aiogram.types as aiogram_types
 from aiogram import Router
+from aiogram.utils.chat_action import ChatActionSender
 
 from src.utils.llm import generate
 from src.utils.retriever import retrieve
@@ -9,8 +10,8 @@ router = Router()
 
 @router.message()
 async def answer(message: aiogram_types.Message):
-    information = await retrieve(message.text)
-    
-    response = await generate(message.text, information)
-    
+    async with ChatActionSender.typing(bot=message.bot, chat_id=message.chat.id):
+        information = await retrieve(message.text)
+        response = await generate(message.text, information)
+
     await message.answer(response)
